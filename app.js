c4eaf6e6f039aafdf7790599aef6d44c5d5ce99f
Oludamilola Osiname <osinameoludamilola@gmail.com>
2020-05-15 17:54:41 +0200

    Change http to https

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function handleGetRandomQuote() {
-  fetch("http://api.icndb.com/jokes/random")
+  fetch("https://api.icndb.com/jokes/random")
     .then((response) => response.json())
     .then((data) => {
       document.querySelector(".quote-card").innerHTML = data.value.joke;
